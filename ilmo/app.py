d9e66a487fff559b30c88eb19c6d357a56211bae
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2018-02-06 19:48:07 +0200

    Prod changes

--- ilmo/app.py
+++ ilmo/app.py
@@ -22,8 +22,8 @@ from flask_cors import CORS, cross_origin
 from pymongo import MongoClient
 
 # Project
-import utils
-# from server import utils
+# import utils
+from server import utils
 
 # ======================================
 # >>> INITIALIZE
@@ -296,4 +296,4 @@ mail = Mail(app)
 app.register_blueprint(routes)
 application = app
 
-application.run()
+# application.run()
