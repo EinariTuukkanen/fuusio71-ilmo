2d85d45d126e3689ba1c8b963b769204c530aa05
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2018-09-21 15:21:42 +0300

    Disables caching

--- app.py
+++ app.py
@@ -42,6 +42,19 @@ def homepage():
     return render_template('fissio.html')
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    # r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 @app.route('/registration', methods=['GET'])
 @cross_origin(origins='*')
 def registration():
