fcbfede275828ba1622fd8860fb1a2a89326edf1
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2019-02-20 13:31:42 +0200

    Separate registration tables

--- static/js/config.js
+++ static/js/config.js
@@ -5,4 +5,4 @@
 // var API_BASE_URL = 'https://fuusio.pro/api';
 var API_BASE_URL = 'http://localhost:5000/api';
 var SESSION_TIMEOUT = 1800; // seconds
-var MAX_USERS = 120;
+var MAX_USERS = 140;
--- static/js/welcome.js
+++ static/js/welcome.js
@@ -11,8 +11,8 @@
 // }
 
 // Helper function to insert users in html table
-function insertUserToTable(index, table, user, excess, prioLimit) {
-    $("#registeredUsers").find('tbody')
+function insertUserToTable(id, index, user) {
+    $("#" + id).find('tbody')
     .append($('<tr>')
         .append($('<td>')
             .text(function() {
@@ -35,78 +35,119 @@ function insertUserToTable(index, table, user, excess, prioLimit) {
     );
 }
 
+
 // On page load get users and insert them to table
 $(function() {
-    var table = document.getElementById('registeredUsers');
     $.ajax({
         url: API_BASE_URL + '/users',
         type: "GET",
         success: function(response) {
             var rawUsersData = JSON.parse(response);
-            // var preUsers = rawUsersData.filter(
-            //     function(u) {
-            //         return !!u.preRegistration;
-            //     }
-            // );
-            // var preUsers = preUsers.sort(
-            //     function(a, b) {
-            //         return a.timestamp - b.timestamp;
-            //     }
-            // );
 
-            // var regularUsers = rawUsersData.filter(
-            //     function(u) {
-            //         return !u.preRegistration;
-            //     }
-            // );
-            // var regularUsers = regularUsers.sort(
-            //     function(a, b) {
-            //         var aRank = guildStatusRank[a.guildStatus];
-            //         var bRank = guildStatusRank[b.guildStatus];
-            //         if (aRank !== bRank) {
-            //             return aRank - bRank;
-            //         } else {
-            //             return a.timestamp - b.timestamp;
-            //         }
-            //     }
-            // );
-            // var usersData = preUsers.concat(regularUsers);
-
-            // var priorityUsers = usersData.filter(
-            //     function(u) {
-            //         return !!u.preRegistration || u.guildStatus === 'currentMember';
-            //     }
-            // );
-            // var count = priorityUsers.length;
-            // console.log('pre-registration + current members : ' + count);
             var usersData = rawUsersData.sort(
                 function(a, b) {
                     return a.timestamp - b.timestamp;
                 }
             );
-            // if (priorityUsers.length < 456 && (new Date()).getTime() >= 1485770400000) {
-            var now = (new Date()).getTime();
-            // if (now > 1518429600000 && now < 1520027700000) {
+
+            var inviteGuests = rawUsersData.filter(function(a) {
+                return a.guildStatus === 'inviteGuest';
+            });
+
+            var guildMembers = rawUsersData.filter(function(a) {
+                return a.guildStatus === 'currentMember';
+            });
+
             $('#registrationButtonContainer').removeClass('hidden');
-            // }
-            // }
-            // var excess = false;
-            // var prioLimit = false;
-            for (var i = 0; i < usersData.length; i++) {
-                //     if (i >= 455) {
-                //         excess = true;
-                //     }
-                //     if (i == count - 1) {
-                //         prioLimit = true;
-                //     }
-                insertUserToTable(i + 1, table, usersData[i]);
-                // insertUserToTable(i + 1, table, usersData[i], excess, prioLimit);
-                // prioLimit = false;
+
+            for (var i = 0; i < inviteGuests.length; i++) {
+                insertUserToTable('registeredInviteGuests', i + 1, inviteGuests[i]);
             }
-       
+
+            var inviteGuestsCount = inviteGuests.length;
+
+            for (var i = 0; i < guildMembers.length; i++) {
+                insertUserToTable('registeredGuildMembers', i + 1 + inviteGuestsCount, guildMembers[i]);
+            }
         },
         error: function(response) {
             console.error('ERROR', response);
         },
     });
 });
+
+// // On page load get users and insert them to table
+// $(function() {
+//     var table = document.getElementById('registeredUsers');
+//     $.ajax({
+//         url: API_BASE_URL + '/users',
+//         type: "GET",
+//         success: function(response) {
+//             var rawUsersData = JSON.parse(response);
+//             // var preUsers = rawUsersData.filter(
+//             //     function(u) {
+//             //         return !!u.preRegistration;
+//             //     }
+//             // );
+//             // var preUsers = preUsers.sort(
+//             //     function(a, b) {
+//             //         return a.timestamp - b.timestamp;
+//             //     }
+//             // );
+
+//             // var regularUsers = rawUsersData.filter(
+//             //     function(u) {
+//             //         return !u.preRegistration;
+//             //     }
+//             // );
+//             // var regularUsers = regularUsers.sort(
+//             //     function(a, b) {
+//             //         var aRank = guildStatusRank[a.guildStatus];
+//             //         var bRank = guildStatusRank[b.guildStatus];
+//             //         if (aRank !== bRank) {
+//             //             return aRank - bRank;
+//             //         } else {
+//             //             return a.timestamp - b.timestamp;
+//             //         }
+//             //     }
+//             // );
+//             // var usersData = preUsers.concat(regularUsers);
+
+//             // var priorityUsers = usersData.filter(
+//             //     function(u) {
+//             //         return !!u.preRegistration || u.guildStatus === 'currentMember';
+//             //     }
+//             // );
+//             // var count = priorityUsers.length;
+//             // console.log('pre-registration + current members : ' + count);
+//             var usersData = rawUsersData.sort(
+//                 function(a, b) {
+//                     return a.timestamp - b.timestamp;
+//                 }
+//             );
+//             // if (priorityUsers.length < 456 && (new Date()).getTime() >= 1485770400000) {
+//             var now = (new Date()).getTime();
+//             // if (now > 1518429600000 && now < 1520027700000) {
+//             $('#registrationButtonContainer').removeClass('hidden');
+//             // }
+//             // }
+//             // var excess = false;
+//             // var prioLimit = false;
+//             for (var i = 0; i < usersData.length; i++) {
+//                 //     if (i >= 455) {
+//                 //         excess = true;
+//                 //     }
+//                 //     if (i == count - 1) {
+//                 //         prioLimit = true;
+//                 //     }
+//                 insertUserToTable(i + 1, table, usersData[i]);
+//                 // insertUserToTable(i + 1, table, usersData[i], excess, prioLimit);
+//                 // prioLimit = false;
+//             }
+       
+//         },
+//         error: function(response) {
+//             console.error('ERROR', response);
+//         },
+//     });
+// });
